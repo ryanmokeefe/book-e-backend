63276d7cdba7c48bf2c02f6b57ddb5dc3508492d
superbuggy <jpqreichard@gmail.com>
2017-12-19 23:46:28 -0500

    add read controller methods

--- controllers/bookmarks.js
+++ controllers/bookmarks.js
@@ -1,10 +1,20 @@
-const express = require('express')
+const express  = require('express')
+const mongoose = require('../db/connection')
+
+// gets the model from our database connection, for querying and changing data  
+const Bookmark = mongoose.model('Bookmark')
 
 // we are invoking express' Router method to obtain an instance of an express Router 
 // that we will then configure below by attaching (via setter methods) route handling 
-const Router  = express.Router()
+const Router = express.Router()
 
 // set a handler function on the router instance to handle GET requests to '/'
-Router.get('/', (req, res)=>res.send("Hello Bookmarks"))
+Router.get('/api/bookmarks/', (req, res) => {
+  Bookmark.find({}).then(bookmarks => res.json(bookmarks))
+})
+
+Router.get('/api/bookmarks/:title', (req, res) => {
+  Bookmark.findOne({title: req.params.title}).then(bookmark => res.json(bookmark))
+})
 
 module.exports = Router
