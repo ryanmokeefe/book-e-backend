936ab814c8a4c03f4a1f5143f63b121fde4dd157
superbuggy <jpqreichard@gmail.com>
2017-12-20 00:11:16 -0500

    add update

--- controllers/bookmarks.js
+++ controllers/bookmarks.js
@@ -10,16 +10,27 @@ const Router = express.Router()
 
 // set a handler function on the router instance to handle GET requests to '/'
 Router.get('/', (req, res) => {
-  Bookmark.find({}).then(bookmarks => res.json(bookmarks))
+  Bookmark
+    .find({})
+    .then(bookmarks => res.json(bookmarks))
 })
 
 Router.get('/:title', (req, res) => {
-  Bookmark.findOne({title: req.params.title}).then(bookmark => res.json(bookmark))
+  Bookmark
+    .findOne({title: req.params.title})
+    .then(bookmark => res.json(bookmark))
 })
 
 Router.post('/', (req, res) => {
-  console.log(req.body)
-  Bookmark.create(req.body).then(bookmarks => res.json(bookmarks))
+  Bookmark
+    .create(req.body)
+    .then(bookmarks => res.json(bookmarks))
+})
+
+Router.put("/:title", (req, res) => {
+  Bookmark
+    .findOneAndUpdate({ title: req.params.title }, req.body)
+    .then(bookmark => res.json(bookmark))
 })
 
 module.exports = Router
